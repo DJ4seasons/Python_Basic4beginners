2189173c89eefc9eb886c09f78b33dde0ff665d1
Jin <gaiaera@gmail.com>
2019-07-02 11:48:33 -0400

    B03_memmap_update

--- B.Basic_Numpy/B03.binary_memmap.py3.py
+++ B.Basic_Numpy/B03.binary_memmap.py3.py
@@ -18,16 +18,23 @@ def bin_file_read2mtx(fname,dtype=np.float32):
 
     return bin_mat
 
+def memmap_range(nt,nt_chunk):
+    #!!! Provide starting and ending point
+    for n in range(nt//nt_chunk+1):
+        n_ini= n*nt_chunk
+        n_end= min((n+1)*nt_chunk,nt)
+        yield n_ini,n_end
 
 ###---- Main
 
 ##-- Parameters
 nn=1000
+kk=500
 
 data_dir= './data/'
 
 ##-- Build and write a dataset for test
-arr=np.arange(nn)
+arr=np.arange(nn*kk).reshape(nn,kk)
 fnout = data_dir+'temp_bin.f32dat'
 
 if not os.path.isfile(fnout):
@@ -35,17 +42,23 @@ if not os.path.isfile(fnout):
         arr.astype(np.float32).tofile(f)
 
 ##-- Read binary file with np.memmap
-arr2=np.memmap(fnout,dtype=np.float32,mode='r',shape=(nn,))
+arr2=np.memmap(fnout,dtype=np.float32,mode='r',shape=(nn,kk))
 
-print(arr[10:20],arr2[10:20])
+print(arr[10:20,99])
+print(arr2[10:20,99])
 
 del arr2
 
 ##-- Read part of data with 'offset'
-nchunk=100
+nchunk=120
 nbyte=4 #<-- float32 = 4byte
+offset_unit=kk*nbyte
+offset=0 #<-- Starting Point
 
-for i in range(int(nn/nchunk)):
-    small_arr=np.memmap(fnout,dtype=np.float32,mode='r',shape=(nchunk,),offset=nchunk*nbyte*i)
+for nni,nne in memmap_range(nn,nchunk):
+    nsize= nne-nni
+    small_arr=np.memmap(fnout,dtype=np.float32,mode='r',shape=(nsize,kk),offset=offset)
     small_arr=np.array(small_arr)
-    print('i={}, error={}'.format(i,(arr[nchunk*i:nchunk*(i+1)]-small_arr).mean()))
+    print('i={}, size={}, error={}'.format(nne,nsize,(arr[nni:nne,:]-small_arr).mean()))
+
+    offset+=nsize*offset_unit
