62181ee374c516e4330d7fd53a1ee08b9c50dd2c
Jin <gaiaera@gmail.com>
2020-09-01 00:24:18 -0400

    Edited on 2020.0831

--- O.Matplotlib_Application/O08_cartopy_projection_regional_ex1_py3.py
+++ O.Matplotlib_Application/O08_cartopy_projection_regional_ex1_py3.py
@@ -1,9 +1,9 @@
 '''
-Matplotlib Application(7)
-Apply various map projection, global maps
+Matplotlib Application(8)
+Compare map projection for regional maps
 
+PlateCarree
 Orthographic
-Geostationary
 NorthPolarStereo
 SouthPolarStereo
 
@@ -38,12 +38,12 @@ def main():
     ###------
     fig=plt.figure()
     fig.set_size_inches(8.5,6)  ## (xsize,ysize)
-    suptit= 'Cartopy Projection Example2 - Global'
+    suptit= 'Cartopy Projection Example3 - Regional'
     fig.suptitle(suptit,fontsize=17,y=0.98,va='bottom') #stretch='semi-condensed') ## x=0., ha='left'
 
     abc= 'abcdefg'
     left,right,top,bottom=0.05,0.93,0.925,0.12
-    iix=left; gapx=0.02; npnx=2
+    iix=left; gapx=0.02; npnx=3
     lx=(right-iix-gapx*(npnx-1))/float(npnx)
     iiy=top; gapy=0.09; npny=2
     ly=(iiy-bottom-gapy*(npny-1))/float(npny)
@@ -56,34 +56,57 @@ def main():
     clevels= np.linspace(0,30,91)
 
     ###--- Map Projection
-    center= 180  # Want to draw a map where dateline is on the center
-    proj_nms= ['Orthographic(ctr_lat=30)','NorthPolarStereo','NearsidePerspective(ctr_lat=30)','SouthPolarStereo']
-    projs = [ccrs.Orthographic(central_longitude=center,central_latitude=30),
-            ccrs.NorthPolarStereo(central_longitude=center),
-            ccrs.NearsidePerspective(central_longitude=center,central_latitude=30),
-            ccrs.SouthPolarStereo(central_longitude=center)]
+    ##-- A region in the Northern Hemisphere
+    map_bound= [140,200,25,65]
+    center= [(map_bound[0]+map_bound[1])/2,(map_bound[2]+map_bound[3])/2]
+    proj_nms= ['PlateCarree','Orthographic','NorthPolarStereo']
+    projs = [ccrs.PlateCarree(central_longitude= center[0]),
+            ccrs.Orthographic(central_longitude= center[0],central_latitude= center[1]),
+            ccrs.NorthPolarStereo(central_longitude=center[0])]
     data_crs= ccrs.PlateCarree()
 
     props_contour= dict(cmap=cm,alpha=0.9,transform=data_crs,extend='both')
 
-    ##-- Top Panel
-    for i in range(npnx*npny):
+    ##-- Top Panels for the NH
+    for i in range(npnx):
         ax1=fig.add_axes([ix,iy,lx,ly],projection=projs[i])  ### Now it's GeoAxes, not just Axes
-        if proj_nms[i]=='NorthPolarStereo':
-            ax1.set_extent([-180,179.9,0,90],crs=data_crs)
-        elif proj_nms[i]=='SouthPolarStereo':
-            ax1.set_extent([-180,179.9,-90,0],crs=data_crs)
+        ax1.set_extent(map_bound,crs=data_crs)
 
         pic1= ax1.contourf(xlon,ylat,sstmean1,clevels,**props_contour)
         ax1.add_feature(LAND)
-        ax1.coastlines(resolution='110m',color='0.25',linewidth=1.)
-        ax1.gridlines(crs=data_crs, linewidth=0.8, color='gray', alpha=0.5, linestyle='--')
+        ax1.coastlines(resolution='50m',color='0.25',linewidth=1.)
+        gl= ax1.gridlines(crs=data_crs, linewidth=0.8, color='gray', alpha=0.5, linestyle='--')
+        gl.xlocator = MultipleLocator(15)
+        gl.ylocator = MultipleLocator(10)
         subtit= '({}) {}'.format(abc[i],proj_nms[i])
         ax1.set_title(subtit,fontsize=13,ha='left',x=0.0)
 
         ix=ix+lx+gapx
         if ix+lx>1:
             iy=iy-ly-gapy; ix=left
+
+    ##-- A region in the Southern Hemisphere
+    map_bound= [140,200,-65,-25]
+    center= [(map_bound[0]+map_bound[1])/2,(map_bound[2]+map_bound[3])/2]
+    proj_nms= ['PlateCarree','Orthographic','SouthPolarStereo']
+    projs = [ccrs.PlateCarree(central_longitude= center[0]),
+            ccrs.Orthographic(central_longitude= center[0],central_latitude= center[1]),
+            ccrs.SouthPolarStereo(central_longitude=center[0])]
+
+    ##-- Bottom Panels for the SH
+    for i in range(npnx):
+        ax1=fig.add_axes([ix,iy,lx,ly],projection=projs[i])  ### Now it's GeoAxes, not just Axes
+        ax1.set_extent(map_bound,crs=data_crs)
+
+        pic1= ax1.contourf(xlon,ylat,sstmean1,clevels,**props_contour)
+        ax1.add_feature(LAND)
+        ax1.coastlines(resolution='50m',color='0.25',linewidth=1.)
+        gl= ax1.gridlines(crs=data_crs, linewidth=0.8, color='gray', alpha=0.5, linestyle='--')
+        gl.xlocator = MultipleLocator(15)
+        gl.ylocator = MultipleLocator(10)
+        subtit= '({}) {}'.format(abc[i+npnx],proj_nms[i])
+        ax1.set_title(subtit,fontsize=13,ha='left',x=0.0)
+        ix=ix+lx+gapx
 
     cb= fns.draw_colorbar(fig,ax1,pic1,size='page',type='horizontal',width=0.025,gap=0.07)
     cb.set_ticks(MultipleLocator(5))
@@ -92,7 +115,7 @@ def main():
 
     ### Show or Save
     outdir= '../Pics/'
-    outfnm= outdir+'O07.cartopy_global_projections_part2.png'
+    outfnm= outdir+'O08.cartopy_projections_regional_ex1.png'
     print(outfnm)
     plt.savefig(outfnm,bbox_inches='tight',dpi=150)
     #plt.savefig(outfnm,dpi=100)
